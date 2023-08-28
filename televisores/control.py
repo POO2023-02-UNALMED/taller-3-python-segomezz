2f94f31fe19d64a86f315b62be8b1065378b4122
segomezz <141938389+segomezz@users.noreply.github.com>
2023-08-27 20:16:46 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -9,22 +9,22 @@ class Control:
     def turnOn(self):
         self.tv.turnOn()
     def canalUp(self):
-        if self.tv._estado:
+        if self.tv.getEstado():
             self.tv.canalUp()
     def canalDown(self):
-        if self.tv._estado:
+        if self.tv.getEstado():
             self.tv.canalDown()
     def volumenUp(self):
-        if self.tv._estado:
+        if self.tv.getEstado():
             self.tv.volumenUp()
     def volumenDown(self):
-        if self.tv._estado:
+        if self.tv.getEstado():
             self.tv.volumenDown()
     def setCanal(self,nuevoCanal):
         TV.setCanal=nuevoCanal
     def setVolumen(self,nuevoVolumen):
         TV.setVolumen=nuevoVolumen
-    def setTv(self,TV):
-        self._tv=TV
+    def setTv(self,tv):
+        self._tv=tv
     def getTv(self,tv):
         return tv
